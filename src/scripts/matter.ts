a785cc823adb2cfd8e36a45747e45900e3abafb0
Anna <anya-kov@yandex-team.ru>
2024-10-13 17:33:40 +0400

    add boilerplate

---
+++ src/scripts/matter.ts
@@ -0,0 +1,84 @@
+import Matter from "matter-js";
+
+export function doIt() {
+  const container = document.querySelector("#physics-container");
+  if (!container) return;
+
+  const groundThickness = 60;
+  const engine = Matter.Engine.create();
+
+  const render = Matter.Render.create({
+    element: container as HTMLElement,
+    engine: engine,
+    options: {
+      width: container.clientWidth,
+      height: container.clientHeight,
+      wireframes: false,
+      background: "transparent",
+    },
+  });
+
+  const boxA = Matter.Bodies.rectangle(400, 200, 80, 80);
+  const boxB = Matter.Bodies.rectangle(450, 50, 80, 80);
+  const ground = Matter.Bodies.rectangle(
+    container.clientWidth / 2,
+    container.clientHeight + groundThickness / 2,
+    10000, // could be container.clientWidth. changed to support window resize
+    groundThickness,
+    {
+      isStatic: true,
+    },
+  );
+  const leftWall = Matter.Bodies.rectangle(
+    0 - groundThickness / 2,
+    container.clientHeight / 2,
+    groundThickness,
+    container.clientHeight * 5,
+    { isStatic: true },
+  );
+  const rightWall = Matter.Bodies.rectangle(
+    container.clientWidth + groundThickness / 2,
+    container.clientHeight / 2,
+    groundThickness,
+    container.clientHeight * 5,
+    { isStatic: true },
+  );
+
+  Matter.Composite.add(engine.world, [boxA, boxB, ground, leftWall, rightWall]);
+
+  const mouse = Matter.Mouse.create(render.canvas);
+  const mouseConstraint = Matter.MouseConstraint.create(engine, {
+    mouse,
+    constraint: {
+      stiffness: 0.2,
+      render: { visible: true },
+    },
+  });
+  Matter.Composite.add(engine.world, mouseConstraint);
+
+  Matter.Render.run(render);
+  const runner = Matter.Runner.create();
+  Matter.Runner.run(runner, engine);
+
+  function handleResize(c: HTMLElement) {
+    render.canvas.width = c.clientWidth;
+    render.canvas.height = c.clientHeight;
+    Matter.Body.setPosition(
+      ground,
+      Matter.Vector.create(
+        c.clientWidth / 2,
+        c.clientHeight + groundThickness / 2,
+      ),
+    );
+    Matter.Body.setPosition(
+      rightWall,
+      Matter.Vector.create(
+        c.clientWidth + groundThickness / 2,
+        c.clientHeight / 2,
+      ),
+    );
+  }
+  window.addEventListener("resize", () =>
+    handleResize(container as HTMLElement),
+  );
+}
